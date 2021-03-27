fa5f56945b6c452ba24f6faaff11bae4a50229b7
modopo <modopo@gmail.com>
2021-03-27 00:14:51 -0700

    Message JSON and reruns allowed

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,4 +1,5 @@
 const readline = require('readline-sync');
+const MESSAGE = require('./calculator_messages.json');
 
 function prompt(message) {
     console.log(`=> ${message}`);
@@ -8,46 +9,55 @@ function invalidNumber(number) {
     return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-prompt('Welcome to Calculator!');
+prompt(MESSAGE['welcome']);
 
-prompt("What's the first number?");
-let number1 = readline.question();
+while (true) {
 
-while (invalidNumber(number1)) {
-    prompt("Hmm... that doesn't look like a valid number.");
-    number1 = readline.question();
-}
+    prompt(MESSAGE['firstNumber']);
+    let number1 = readline.question();
 
-prompt("What's the second number?");
-let number2 = readline.question();
+    while (invalidNumber(number1)) {
+        prompt(MESSAGE['notValidNumber']);
+        number1 = readline.question();
+    }
 
-while (invalidNumber(number2)) {
-    prompt("Hmm... that doesn't look like a valid number.");
-    number2 = readline.question();
-}
+    prompt(MESSAGE['secondNumber']);
+    let number2 = readline.question();
 
-prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operation = readline.question();
+    while (invalidNumber(number2)) {
+        prompt(MESSAGE['notValidNumber']);
+        number2 = readline.question();
+    }
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt('Must choose 1, 2, 3 or 4')
-    operation = readline.question();
-}
+    prompt(MESSAGE['operations']);
+    let operation = readline.question();
 
-let output;
-switch (operation) {
-    case '1':
-        output = Number(number1) + Number(number2);
+    while (!['1', '2', '3', '4'].includes(operation)) {
+        prompt(MESSAGE['notValidOperator'])
+        operation = readline.question();
+    }
+
+    let output;
+    switch (operation) {
+        case '1':
+            output = Number(number1) + Number(number2);
+            break;
+        case '2':
+            output = Number(number1) - Number(number2);
+            break;
+        case '3':
+            output = Number(number1) * Number(number2);
+            break;
+        case '4':
+            output = Number(number1) / Number(number2);
+            break;
+    }
+
+    prompt(`The result is: ${output}`);
+    prompt(MESSAGE['rerun']);
+    let rerun = readline.question();
+    
+    if (rerun[0].toLowerCase() === 'n') {
         break;
-    case '2':
-        output = Number(number1) - Number(number2);
-        break;
-    case '3':
-        output = Number(number1) * Number(number2);
-        break;
-    case '4':
-        output = Number(number1) / Number(number2);
-        break;
+    }
 }
-
-prompt(`The result is: ${output}`);
