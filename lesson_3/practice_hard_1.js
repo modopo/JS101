3181fa0a8e337b17310e7f190270380ee38264b3
modopo <modopo@gmail.com>
2021-04-10 23:31:19 -0700

    Hard complete

---
+++ lesson_3/practice_hard_1.js
@@ -0,0 +1,30 @@
+//Question 4
+function isDotSeparatedIpAddress(inputString) {
+    let dotSeparatedWords = inputString.split(".");
+
+    if (dotSeparatedWords.length !== 4) {
+        return false
+    }
+
+    while (dotSeparatedWords.length > 0) {
+        let word = dotSeparatedWords.pop();
+        if (!isAnIpNumber(word)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function isAnIpNumber(str) {
+    if (/^\d+$/.test(str)) {
+        let number = Number(str);
+        return number >= 0 && number <= 255;
+    }
+
+    return false;
+}
+
+console.log(isDotSeparatedIpAddress("4.5.6.7"));
+console.log(isDotSeparatedIpAddress("4.5.6"));
+console.log(isDotSeparatedIpAddress("4.5"));
