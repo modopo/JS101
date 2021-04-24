25748823ce5b02d3ac7c64f18992df976d8cd0db
modopo <modopo@gmail.com>
2021-04-23 20:52:48 -0700

    Lesson 4 completed

---
+++ lesson_4/pp11.js
@@ -0,0 +1,20 @@
+let statement = "The Flintstones Rock";
+
+function flatten (phrase) {
+    let result = {};
+    let arr = phrase.split('').filter(char => {
+        return char !== ' ';
+    });
+
+    arr.forEach(letter => {
+        if (result.hasOwnProperty(letter)) {
+            result[letter] += 1;
+        } else {
+            result[letter] = 1;
+        }
+    });
+
+    return result;
+}
+
+console.log(flatten(statement));
