0bd4c79511ded3acca1eb720d2178dd11cf423a2
modopo <modopo@gmail.com>
2021-05-16 16:31:16 -0700

    Refactored and ESlinted

--- lesson_6/twenty_one.js
+++ lesson_6/twenty_one.js
@@ -1,11 +1,12 @@
 const readline = require('readline-sync');
 const FULL_DECK = {
-  C: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
-  D: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
-  H: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
-  S: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+  "♣": [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
+  "♦": [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
+  "♥": [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
+  "♠": [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
 };
-const FACE_CARD_NAMES = [['J', 'Jack'], ['Q', 'Queen'], ['K', 'King'], ['A', 'Ace']];
+const FACE_CARD_NAMES = [['J', 'Jack'], ['Q', 'Queen'],
+  ['K', 'King'], ['A', 'Ace']];
 const STARTING_CARD_NUMBER = 2;
 const DEALER_MIN = 17;
 const BEST_HAND_VALUE = 21;
@@ -36,7 +37,7 @@ function randomIndex(arr) {
 }
 
 function justNumericalValue(table, side) {
-  return table[side].map(card => justFaceNames(card[1]));
+  return table[side].map(card => justFaceNames(card[1]) + card[0]);
 }
 
 function justFaceNames(cardValue) {
@@ -145,17 +146,25 @@ function determineBust(table, side) {
   return total > BEST_HAND_VALUE;
 }
 
-function determineWinner(table) {
-  let playerTotal = calculateTotal(table['player']);
-  let dealerTotal = calculateTotal(table['dealer']);
-
+function determineWinner(table, score, playerTotal, dealerTotal) {
   if (playerTotal > dealerTotal) {
-    return 'player';
+    updateScoreToWinner(score, 'player');
+    displayHands(table, score, false);
+    prompt(`Your total is ${playerTotal}` +
+      ` and the dealer's total is ${dealerTotal}`);
+    prompt("The winnner is the Player!");
   } else if (playerTotal < dealerTotal) {
-    return 'dealer';
+    updateScoreToWinner(score, 'dealer');
+    displayHands(table, score, false);
+    prompt(`Your total is ${playerTotal}` +
+      ` and the dealer's total is ${dealerTotal}`);
+    prompt("The winner is the Dealer!");
+  } else {
+    displayHands(table, score, false);
+    prompt(`Your total is ${playerTotal}` +
+      ` and the dealer's total is ${dealerTotal}`);
+    prompt("It was a tie!");
   }
-
-  return '';
 }
 
 function determineMatch(score) {
@@ -171,21 +180,31 @@ function determineMatch(score) {
 }
 
 //Interface---------------------------------------------------------------------
+function welcome() {
+  console.clear();
+  console.log("Welcome to a game of Twenty-One." +
+    "First to reach 5 points wins the match.");
+  console.log("Press [Enter] to continue.");
+  readline.question();
+}
+
 function displayHands(table, score, hidden = true) {
   console.clear();
   displayScore(score);
 
+  let dealerFirstCard = justNumericalValue(table, 'dealer')[0];
+  let dealerUnknownCards = justNumericalValue(table, 'dealer').length - 1;
+  let dealerFull = joinWithDelimiters(justNumericalValue(table, 'dealer'));
+  let playerFull = joinWithDelimiters(justNumericalValue(table, 'player'));
+
   if (hidden) {
-    console.log(`Dealer has: ${justFaceNames(table.dealer[0][1])} 
-      and (${justNumericalValue(table, 'dealer').length - 1}) unknown card(s)`);
-    console.log(`Player has: 
-      ${joinWithDelimiters(justNumericalValue(table, 'player'))}`);
+    console.log(`Dealer has: ${dealerFirstCard} and ` +
+      `(${dealerUnknownCards}) unknown card(s)`);
+    console.log(`Player has: ${playerFull}`);
     console.log('-----------------------------------');
   } else {
-    console.log(`Dealer has: 
-      ${joinWithDelimiters(justNumericalValue(table, 'dealer'))}`);
-    console.log(`Player has: 
-      ${joinWithDelimiters(justNumericalValue(table, 'player'))}`);
+    console.log(`Dealer has: ${dealerFull}`);
+    console.log(`Player has: ${playerFull}`);
     console.log('-----------------------------------');
   }
 }
@@ -193,70 +212,93 @@ function displayHands(table, score, hidden = true) {
 //Actions for Game Flow--------------------------------------------------------
 function hitOrStay() {
   prompt("Hit (h) or Stay (s)? Please choose 'h' or 's'.");
-  let answer = readline.question().toLowerCase()[0];
+  let answer = readline.question().toLowerCase();
   while (answer !== 'h' && answer !== 's') {
-    prompt("Sorry, that's an invalid input. Please choose 'h' to hit or 's' to stay.");
-    answer = readline.question().toLowerCase()[0];
+    prompt("Sorry, that's an invalid input. Please choose " +
+      "'h' to hit or 's' to stay.");
+    answer = readline.question().toLowerCase();
   }
 
   return answer;
 }
 
-function beginDealerTurn(table, deck) {
-  prompt("Player has chosen to stay.");
-  prompt("Press [ENTER] to see what the Dealer does...");
-  readline.question();
-
-  dealerPlays(table, deck);
-}
-
-function dealerPlays(table, deck) {
+function dealerTurn(table, deck, score) {
   while (calculateTotal(table['dealer']) < DEALER_MIN) {
     addCardToHand(table, selectRandomCard(deck), 'dealer');
+  }
+
+  dealerEndSequence(table, score, determineBust(table, 'dealer'));
+
+}
+
+function dealerEndSequence(table, score, bust) {
+  let playerTotal = calculateTotal(table['player']);
+  let dealerTotal = calculateTotal(table['dealer']);
+
+  if (bust) {
+    updateScoreToWinner(score, 'player');
+    displayHands(table, score, false);
+    prompt("The dealer was dealt" +
+      ` ${justNumericalValue(table, 'dealer').length - 2} card(s).`);
+    prompt(`The dealer's total is ${dealerTotal} ` +
+      `and exceeded ${BEST_HAND_VALUE}.`);
+    prompt(`The winner is the Player!`);
+
+  } else if (!bust) {
+    revealWinner(table, score);
+    determineWinner(table, score, playerTotal, dealerTotal);
   }
 }
 
 function revealWinner(table, score) {
-  displayHands(table, score, true);
-  prompt(`The Dealer has chosen to stay after being dealt ${justNumericalValue(table, 'dealer').length - 2} card.`);
+  displayHands(table, score);
+  prompt("The Dealer has chosen to stay after being dealt" +
+    ` ${justNumericalValue(table, 'dealer').length - 2} card.`);
   prompt("Press [ENTER] to reveal the winner!");
   readline.question();
 }
 
-function endSequence(table, score, winner, busted = false) {
-  updateScoreToWinner(score, winner);
-  displayHands(table, score, false);
-
-  let playerTotal = calculateTotal(table['player']);
-  let dealerTotal = calculateTotal(table['dealer']);
-
-  if (busted) {
-    if (winner === 'dealer') {
-      prompt(`Your total is ${playerTotal} and exceeded ${BEST_HAND_VALUE}.`);
-    } else if (winner === 'player') {
-      prompt(`The dealer was dealt ${justNumericalValue(table, 'dealer').length - 2} card(s).`);
-      prompt(`The dealer's total is ${dealerTotal} and exceeded ${BEST_HAND_VALUE}.`);
+function playerTurn(table, deck, score) {
+  while (true) {
+    let answer = hitOrStay();
+    if (answer === 'h') {
+      addCardToHand(table, selectRandomCard(deck), 'player');
+      displayHands(table, score);
     }
-    prompt(`The winnner is the ${winner}!`);
+
+    let playerBusted = determineBust(table, 'player');
+    if (answer === 's' || playerBusted) {
+      break;
+    }
   }
 
-  if (!busted) {
-    prompt(`Your total is ${playerTotal} and the dealer's total is ${dealerTotal}`);
-    if (playerTotal === dealerTotal) {
-      prompt("It was a tie!");
-    } else {
-      prompt(`The winnner is the ${winner}!`);
-    }
+  playerEndSequence(table, score, 'dealer', determineBust(table, 'player'));
+}
+
+function playerEndSequence(table, score, winner, busted = false) {
+  displayHands(table, score);
+
+  let playerTotal = calculateTotal(table['player']);
+
+  if (busted) {
+    updateScoreToWinner(score, winner);
+    displayHands(table, score, false);
+    prompt(`Your total is ${playerTotal} and exceeded ${BEST_HAND_VALUE}.`);
+    prompt(`The winner is the ${winner}!`);
+  } else if (!busted) {
+    prompt("Player has chosen to stay.");
+    prompt("Press [ENTER] to see what the Dealer does...");
+    readline.question();
   }
 }
 
 function playAgain() {
   prompt("Try again? (y/n)");
-  let answer = readline.question().toLowerCase()[0];
+  let answer = readline.question().toLowerCase();
   while (answer !== 'y' && answer !== 'n') {
     prompt("Sorry, that's an invalid input. Please choose between 'y' or 'n'");
     prompt("Try again? (y/n)");
-    answer = readline.question().toLowerCase()[0];
+    answer = readline.question().toLowerCase();
   }
 
   return answer;
@@ -264,41 +306,19 @@ function playAgain() {
 
 //Main
 while (true) {
+  welcome();
   let score = initScore();
 
   while (score.player !== POINTS_TO_WIN_MATCH ||
     score.dealer !== POINTS_TO_WIN_MATCH) {
     let deck = initDeck();
     let table = initTable(deck);
+    displayHands(table, score);
 
-    while (true) {
-      displayHands(table, score);
+    playerTurn(table, deck, score);
 
-      let answer = hitOrStay();
-      if (answer === 'h') {
-        addCardToHand(table, selectRandomCard(deck), 'player');
-        displayHands(table, score);
-      }
-
-      let playerBusted = determineBust(table, 'player');
-
-      if (playerBusted) {
-        endSequence(table, score, 'dealer', playerBusted);
-        break;
-      } else if (answer === 's') {
-        beginDealerTurn(table, deck);
-
-        let dealerBusted = determineBust(table, 'dealer');
-
-        if (dealerBusted) {
-          endSequence(table, score, 'player', dealerBusted);
-          break;
-        } else {
-          revealWinner(table, score);
-          endSequence(table, score, determineWinner(table));
-        }
-        break;
-      }
+    if (!determineBust(table, 'player')) {
+      dealerTurn(table, deck, score);
     }
 
     score = determineMatch(score);
