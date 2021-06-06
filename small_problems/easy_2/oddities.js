2b45eeb8e8ef20468af80cfefe81b027c7c6c511
modopo <modopo@gmail.com>
2021-06-06 00:24:46 -0700

    Odd elements in array only

---
+++ small_problems/easy_2/oddities.js
@@ -0,0 +1,15 @@
+function oddities(var1) {
+    let result = [];
+
+    if (var1.length === 0) {
+        return result;
+    }
+
+    for (let index = 0; index <= var1.length; index += 2) {
+        result.push(var1[index]);
+    }
+
+    return result;
+}
+
+console.log(oddities([]));
