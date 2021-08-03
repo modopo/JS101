64447d5aceec85283932fb83f259afaaa8041cdf
modopo <modopo@gmail.com>
2021-08-02 21:49:26 -0700

    Completed Easy 3

--- small_problems/easy_3/century.js
+++ small_problems/easy_3/century.js
@@ -1,13 +1,31 @@
 function century(input) {
-    let century = 0;
+    let century = Math.floor(input / 100) + 1
 
     if (input % 100 === 0) {
-        century = input % 100;
-    } else {
-        century = (input / 100) + 1;
+        century -= 1;
     }
 
-    return century;
+    return String(century) + centurySuffix(century);
 }
 
-console.log(century(2001));
+function centurySuffix(century) {
+    if (exceptionTh(century % 100)) {
+        return 'th';
+    }
+
+    let lastDigit = century % 10;
+
+    switch (lastDigit) {
+        case 1: return 'st';
+        case 2: return 'nd';
+        case 3: return 'rd';
+        default: return 'th';
+    }
+}
+
+function exceptionTh(lastTwo) {
+    return lastTwo === 11 || lastTwo === 12 || lastTwo === 13;
+}
+
+console.log(century(101));
+console.log(century(201));
