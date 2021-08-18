96e0eaee3110386f05efed024c5a3fac0eed78ba
modopo <modopo@gmail.com>
2021-08-18 00:33:43 -0700

    Upgrade palindrome

--- small_problems/easy_4/palindrome.js
+++ small_problems/easy_4/palindrome.js
@@ -1,5 +1,28 @@
+function isRealPlanidrom(input) {
+    input = enforceAlphanumericOnly(input.toLowerCase());
+    return isPalindrome(input);
+}
+
 function isPalindrome(input) {
     return input === input.split('').reverse().join('');
 }
 
-console.log(isPalindrome('madam'));
+function enforceAlphanumericOnly(string) {
+    let arr = string.split('');
+    let result = '';
+
+    arr.forEach(char => {
+        if (isAlphanumeric(char)) {
+            result += char;
+        }
+    });
+
+    return result;
+}
+
+function isAlphanumeric(char) {
+    return (char >= 'a' && char <= 'z') || (char >= 0 && char <= 9)
+}
+
+console.log(isRealPlanidrom('Mad|am'));
+console.log(isRealPlanidrom('123a321'));
