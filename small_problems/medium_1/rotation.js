51c2f8a9fb714a47c8b0bb8029957c33f41256fb
modopo <modopo@gmail.com>
2021-11-21 00:20:48 -0800

    Rotating first array

---
+++ small_problems/medium_1/rotation.js
@@ -0,0 +1,13 @@
+function rotateArray(array) {
+    if (!Array.isArray(array)) {
+        return undefined;
+    }
+
+    if (array.length === 0) {
+        return [];
+    }
+
+    return array.slice(1).concat(array[0]);
+}
+
+console.log(rotateArray([1,2,3,4]))
