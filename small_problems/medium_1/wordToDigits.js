759ad72c745a4ca27fc36e17a5f2d8497358ac04
modopo <modopo@gmail.com>
2021-12-18 16:21:11 -0800

    Used regex instead

--- small_problems/medium_1/wordToDigits.js
+++ small_problems/medium_1/wordToDigits.js
@@ -1,15 +1,12 @@
 function wordToDigitis(sentence) {
     const NUM_WORDS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
-    let result = sentence.split(' ').map(word => {
-        if (NUM_WORDS.indexOf(word) >= 0) {
-            return String(NUM_WORDS.indexOf(word));
-        } else {
-            return word;
-        }
-    }).join(' ');
+    NUM_WORDS.forEach(word => {
+        let regex = RegExp(word, 'g');
+        sentence = sentence.replace(regex, NUM_WORDS.indexOf(word))
+    });
 
-    return result;
+    return sentence;
 }
 
 console.log(wordToDigitis('one two three four five six seven eight. thanks.'));
