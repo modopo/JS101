8eeed047599d86f057594557ae7ea5c640cf5fe8
modopo <modopo@gmail.com>
2022-03-12 15:53:53 -0800

    Transpose MxN matrix

--- small_problems/adv_1/transpose.js
+++ small_problems/adv_1/transpose.js
@@ -1,28 +1,4 @@
 const matrix = [
-<<<<<<< HEAD
-    [1, 2, 3],
-    [4, 5, 5],
-    [7, 8, 9]
-];
-
-function transpose(arr) {
-    let result = [];
-    for (let idx = 0; idx < arr[0].length; idx++) {
-        result.push([]);
-    }
-
-    for (let outer = 0; outer < arr.length; outer++) {
-        for (let inner = 0; inner < arr[outer].length; inner++) {
-            result[inner].push(arr[outer][inner])
-        }
-    }
-
-    return result;
-}
-
-let newMatrix = transpose(matrix);
-
-=======
     [1, 5, 8, 5],
     [4, 7, 2, 0],
     [3, 9, 6, 1]
@@ -46,5 +22,4 @@ function transpose(arr) {
     return template;
 }
 
->>>>>>> main
 console.log(newMatrix);
